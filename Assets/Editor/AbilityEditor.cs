5e68f00a3c261030a949d1b42ded759f337fd545
C.Rondeau <rondeau.clement@gmail.com>
2018-04-12 08:10:11 +0200

    Add custom inspector for ability

---
+++ Assets/Editor/AbilityEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Ability))]
+public class AbilityEditor : Editor
+{
+    Ability ability;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Change things"))
+        {
+            Debug.Log("Debug");
+        }
+    }
+}
