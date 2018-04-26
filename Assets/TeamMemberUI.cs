94775c8526ee8f9b9c260a320999e65f91227e68
C.Rondeau <rondeau.clement@gmail.com>
2018-04-26 23:55:43 +0200

    Add structure for part management UI

---
+++ Assets/TeamMemberUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TeamMemberUI : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    private Button _button;
+
+    void Start ()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnClick);
+        // TODO : Set color to unselected except for first one
+    }
+
+    private void OnClick()
+    {
+        TeamUIManager.instance.SwitchToMemberCustomization();
+    }
+	
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        // TODO : Manage color selection here, because Unity    
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        // TODO : Update team member info panel
+    }
+}
---
+++ Assets/TeamUIManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TeamUIManager : MonoBehaviour {
+
+    public static TeamUIManager instance
+    {
+        get
+        {
+            if (!_teamUIManager)
+            {
+                _teamUIManager = FindObjectOfType(typeof(TeamUIManager)) as TeamUIManager;
+
+                if (!_teamUIManager)
+                {
+                    Debug.LogError("There needs to be one active TeamUIManager script on a GameObject in your scene.");
+                }
+            }
+
+            return _teamUIManager;
+        }
+    }
+
+    public CanvasGroup skillsetEditor;
+    public CanvasGroup teamUI;
+    public CanvasGroup membersList;
+    public CanvasGroup memberCustomization;
+    public CanvasGroup availablePartsList;
+
+    public GameObject firstSelectedPart;
+
+    public GameObject firstSelectedAvailablePart;
+
+    private static TeamUIManager _teamUIManager;
+    private bool _playerChoiceMade = false;
+    private Coroutine _waitingRoutine = null;
+
+    public void Show()
+    {
+        // TODO : Set focus on first member
+    }
+
+    public void Hide()
+    {
+        skillsetEditor.alpha = 1;
+        skillsetEditor.interactable = true;
+
+        teamUI.alpha = 0;
+        teamUI.interactable = false;
+    }
+
+    public void SwitchToMemberCustomization()
+    {
+        memberCustomization.interactable = true;
+        membersList.interactable = false;
+
+        EventSystem.current.SetSelectedGameObject(firstSelectedPart);
+    }
+
+    public void ShowPartsList()
+    {
+        availablePartsList.alpha = 1;
+        availablePartsList.interactable = true;
+
+        memberCustomization.interactable = false;
+
+        EventSystem.current.SetSelectedGameObject(firstSelectedAvailablePart);
+
+        _waitingRoutine = StartCoroutine(WaitForPlayerChoice());
+    }
+
+    public void HidePartsList()
+    {
+        availablePartsList.alpha = 0;
+        availablePartsList.interactable = false;
+
+        memberCustomization.interactable = true;
+
+        EventSystem.current.SetSelectedGameObject(firstSelectedPart);
+    }
+
+    private IEnumerator WaitForPlayerChoice()
+    {
+        while (!_playerChoiceMade)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                // TODO
+
+                if(availablePartsList.interactable)
+                {
+                    HidePartsList();
+                }
+                
+                break;
+            }
+
+            yield return null;
+        }
+    }
+}
